2067e7e3bb881a6148ad3a0673da378ed6f45bf2
mdalcaraz <mdalcaraz@live.com>
2023-11-01 08:54:20 -0300

    Livewire installation

---
+++ app/Http/Livewire/ViewOrders.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ViewOrders extends Component
+{
+    public function render()
+    {
+        return view('livewire.view-orders');
+    }
+}
