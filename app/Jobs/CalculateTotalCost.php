77a8ba005846567f7452850335806631e2266f5d
mdalcaraz <mdalcaraz@live.com>
2023-11-01 09:36:23 -0300

    Added Calculate Total Cost job

---
+++ app/Jobs/CalculateTotalCost.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+use App\Models\OrderLine;
+
+
+class CalculateTotalCost implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        sleep(10); //Verificacion de Asincronismo
+        $totalCost = 0;
+        $orderLines = OrderLine::with('order', 'product')->get();
+        foreach ($orderLines as $line) {
+            $totalCost += $line->qty * $line->product->cost;
+        }
+        echo "Total Cost: " . $totalCost;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Jobs\CalculateTotalCost;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    CalculateTotalCost::dispatch();
     return view('welcome');
 });
