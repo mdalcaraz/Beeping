b17537a0ef0643753361ca2d3b4ae9d8b00d85eb
mdalcaraz <mdalcaraz@live.com>
2023-11-01 09:52:12 -0300

    Add Table

--- resources/views/livewire/view-orders.blade.php
+++ resources/views/livewire/view-orders.blade.php
@@ -1,4 +1,21 @@
 <div class="container">
-
+    <table class="table table-dark">
+        <thead>
+            <tr>
+                <th scope="row">Oder Ref</th>
+                <th>Customer Name</th>
+                <th>Total Qty</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($orderTotals as $orderTotal)
+                <tr>
+                    <td>{{ $orderTotal->order_ref }}</td>
+                    <td>{{ $orderTotal->customer_name }}</td>
+                    <td>{{ $orderTotal->total_qty }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
 
